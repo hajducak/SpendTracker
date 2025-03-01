4af94e6bd0646d09e10ee157854c9bb82476766e
Marek Hajdučák <hajducakmarek@gmail.com>
2025-03-01 18:27:41 +0100

    Initial project setup

---
+++ SpendTracker/SpendTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendTrackerApp.swift
+//  SpendTracker
+//
+//  Created by MacBook Pro on 01/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct SpendTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
